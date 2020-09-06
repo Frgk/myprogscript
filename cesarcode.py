743eab3886898fab2817b2fdb6302fe442f514e6
Frgk <69361571+Frgk@users.noreply.github.com>
2020-09-06 18:47:15 +0200

    Create cesarcode.py

---
+++ cesarcode.py
@@ -0,0 +1,35 @@
+
+
+mess = input("Entrez le message à coder : ")    #Enter the text to code
+key = int(input("Entrez la clé du chiffrement : ")) #Enter the key
+
+alphabet = ['a', 'b', 'c', 'd', 'f', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+
+def letter_reco(letter, liste):  #Defining a function used to code a letter
+
+    i = 0
+    while letter != liste[i]:
+        i += 1
+
+    j = i + key
+    if j >= 26:
+        j -= 26
+    elif j < 0:
+        j += 26
+
+    return j
+
+
+def main():
+    new_message = str()
+
+    for i in range(len(mess)):  # execute a "for" loop on each letter of the message
+        new_letter = alphabet[letter_reco(mess[i], alphabet)]
+        new_message += new_letter
+
+    print("Coded message : ",new_message)
+
+
+main()
