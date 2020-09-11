6ec42638a2e50cd746b62b0c87a19db819cf1213
Frgk <69361571+Frgk@users.noreply.github.com>
2020-09-11 20:02:39 +0200

    Create vigenerecode.py
    add a script to code message with the Vigenere algorithm

---
+++ vigenerecode.py
@@ -0,0 +1,37 @@
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+
+text = input("Enter the text to code : ")
+key = input("Enter the key : ")
+
+
+
+def letter_reco(letter, liste):
+    i = 0
+    while letter != liste[i]:
+        i += 1
+    return i
+
+def main():
+    new_message = str()
+    j = 0
+    for i in range(len(text)):
+
+        coord = letter_reco(text[i], alphabet) + letter_reco(key[j], alphabet)
+
+        coord = coord % 26
+
+
+        #print(alphabet[letter_reco(text[i],alphabet)] + " - ",letter_reco(text[i],alphabet)+1)
+        char = alphabet[coord]
+        new_message += char
+
+        if j == len(key)-1:
+            j = 0
+        else :
+            j += 1
+    print("Coded message : " +new_message)
+main()
