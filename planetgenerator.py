fad5fce9215b81c4bcd83c71951c5969be2acea7
Frgk <69361571+Frgk@users.noreply.github.com>
2020-09-12 19:27:24 +0200

    Update planetgenerator.py
    added the size of the planet and his number of moons

--- planetgenerator.py
+++ planetgenerator.py
@@ -1,12 +1,12 @@
 
 
 import random
+
 from termcolor import colored
 
 
 def temp_planet():
     return random.randint(-300, 300)
-
 
 def planet_type(temperature):
     if temperature >= -300 and temperature < -100:
@@ -22,40 +22,43 @@ def planet_type(temperature):
     elif temperature >= 100 and temperature < 300:
         return "Calcined"
 
-def random_name():
-    pre = ["Me","Mo","Ma","Mu","Re","Ra","Ro","Ru","Yo","Pa","Pu","Pr","Za","We","Gr","Gu","Gy","Ko","Ke","Be",]
-    mid = ["is","eos","eor","eo","br","rr","r","mm","m","nn","n","ty","ci","ta","la","pp","p"]
-    end = ["sir","ber","ler","per","era","ero","ro","ver","vo","yst","st","sto"]
+def planet_name(name):
 
+    if name == '':
+        pre = ["Me","Mo","Ma","Mu","Re","Ra","Ro","Ru","Yo","Pa","Pu","Pr","Za","We","Gr","Gu","Gy","Ko","Ke","Be",]
+        mid = ["is","eos","eor","eo","br","rr","r","mm","m","nn","n","ety","ci","ta","la","pp","p"]
+        end = ["sir","ber","ler","per","era","ero","ro","ver","vo","yst","st","sto"]
+        return random.choice(pre) + random.choice(mid) + random.choice(end)
 
-
-    return random.choice(pre) + random.choice(mid) + random.choice(end)
+    else :
+        return name
 
 def planet_size():
 
     return random.randint(1900,70000)      #Based on the size of Kepler 37b(3900km) and Jupiter(139822km)
 
+def number_moons():
+    return random.randint(0, 82)        #Based on the 82 Saturn's moons
+
 
 
 
 def create_planet(name):
-    planet_name = str()
-    if name == '':
-        planet_name = random_name()
-    else:
-        planet_name = name
 
-    print("Planet Name : " + planet_name)
+    planetname = planet_name(name)
+    print("Planet Name : " + planetname)
 
 
-    temp = temp_planet()
+    temperature = temp_planet()
+    print("Temperature on surface : " + str(temperature) +" °C")
+    print("Type of the planet :  " + planet_type(temperature))
 
-    print("Temperature on surface : " + str(temp) +" °C")
-    print("This planet is " + planet_type(temp))
 
+    planetsize = planet_size()
+    print("Planet Diameter : " + str(planetsize) + " km")
 
-    print("Planet Diameter : " + str(planet_size()) + " km")
-
+    numbermoon = number_moons()
+    print("Number of moons : " + str(numbermoon))
 
 
 
@@ -63,6 +66,7 @@ def create_planet(name):
 
 
 
-nom = input("Enter the name of the planet ( Leave it blank for a random name ) : ")
+
+nom = str(input("Enter the name of the planet ( Leave it blank for a random name ) : "))
 
 create_planet(nom)
