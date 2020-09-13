5609a2882442462628a520dccb25dd9daf0ba72e
Frgk <69361571+Frgk@users.noreply.github.com>
2020-09-13 14:13:38 +0200

    Update planetgenerator.py
    Added a generator of story of the planet

--- planetgenerator.py
+++ planetgenerator.py
@@ -46,9 +46,75 @@ def planet_number():
     if chance >=70:
         teles = random.randint(1,22)        #Based on the 22 stars discovered by Kepler
         num = ['b','c','d','e','f','g']     #Based on from Kepler-11b to Kepler-11g
-        return "-"+str(teles)+random.choice(num)
+        return "-"+str(teles)+" "+random.choice(num)
     else:
         return ''
+
+def planet_age():
+    return random.randint(10000,50000000)
+
+
+
+def story_planet(name,number,temperature,type,size,moon,age):
+
+    pre = [
+           "This planet called "+name,
+           "Also called as "+name+", this planet",
+           "This planet discovered by the telescope "+name+",",
+           "We know that "+name,
+           "Situated near our system SOL,"+name,
+           "Aged of "+str(age)+" years,"+ name
+           ]
+
+    pre2 = [
+        "is totally "+type,
+        "is entirely "+type,
+        "has an "+type+" environment",
+        "is in an "+type+" era"
+    ]
+
+    pre3=["and has a temperature surrounding " + str(temperature) + " °C",
+          "with a temperature of " + str(temperature) + " °C",
+          "due to temperatures approaching " + str(temperature) + " °C",
+    ]
+
+    if temperature >= -300 and temperature < 0:
+        mid = [name + " is a telluric planet, covered with ice",
+               "The planet " + name + "is mostly composed of ice",
+               name + " can be considered like a liquid planet, because of the presence of liquid nitrogen on surface",
+               name + " is like Earth, it's a telluric planet, but with ice on surface"
+        ]
+
+    elif temperature >= 0 and temperature < 40:
+        mid=[name + " has a temperature at a temperature that would allow humans to live there",
+           "Thanks to his proximity with his sun, "+name+" is a telluric planet with a correct temperature",
+           name + " is an uncommon planet, with a good ambient temperature but it's a gas planet",
+           "Due to proximity with his star, " + name + " is a liquid planet heat to a boil"
+        ]
+    elif temperature >= 40 and temperature <= 300:
+        mid=[name + " is an old telluric planet that has been heat with the core heat. Now it's a magmatic planet reaching unthinkable temperatures",
+             name + " can be considered like a liquid planet, with the presence of lava on surface"
+        ]
+
+    mid2 = [", with " + str(moon) + "moons orbiting around.",
+            ", counting at least " + str(moon) +" moons around.",
+            ". Moreover, among these " + str(moon) + " moons, one looks like the planet itself, and has an elliptical orbit approaching the planet each year.",
+            ". "+ name + "  has " + str(moon) + " moons having been destroyed by meteorites over the past centuries. These moons are now, fields of lunar debris orbiting around " + name,
+
+
+
+    ]
+
+
+
+
+
+
+    return random.choice(pre) +" "+random.choice(pre2) + " "+random.choice(pre3)+".\n"+random.choice(mid) + random.choice(mid2)
+
+
+
+
 
 def create_planet(name):
 
@@ -67,6 +133,13 @@ def create_planet(name):
 
     numbermoon = number_moons()
     print("Number of moons : " + str(numbermoon))
+
+    planetage = planet_age()
+
+
+
+    planetstory = story_planet(planetname,planetnumber,temperature,planet_type(temperature),planetsize,numbermoon,planetage)
+    print("\n\n"+planetstory)
 
 
 
